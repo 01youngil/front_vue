190af878e0106b75f2a076a70dba213058ab181c
01youngil <cka1782@gmail.com>
2025-02-12 17:19:31 +0900

    20250212

--- src/components/practice/VuetifyComponent.vue
+++ src/components/practice/VuetifyComponent.vue
@@ -60,9 +60,52 @@
             </v-form>
         </v-card-text>
     </v-card>
+    <br><br>
+    <v-btn @click="cancel()">취소버튼</v-btn>
+    <!-- 모달창을 위한 v-dialog -->
+    <v-dialog v-model="trueOrFalse" max-width="400px">
+        <v-card>
+            <v-card-title>
+                정말 취소하시겠습니까?
+            </v-card-title>
+            <v-card-actions>
+                <v-btn color="primary" @click="resetModal()">예</v-btn>
+                <v-btn color="secondary" @click="resetModal()">아니오</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+    <br><br>
+    <v-btn @click="hrefRouting()">일반라우팅</v-btn>
+    <v-btn @click="spaRouting1()">SPA 라우팅(router기능활용)</v-btn>
+    <v-btn @click="spaRouting2()">SPA 라우팅(router기능활용-name호출)</v-btn>
+
 </template>
 <script>
     export default{
-
+        data(){
+            return{
+                trueOrFalse:false,
+            }
+        },
+        methods: {
+            cancel(){
+                this.trueOrFalse = true;
+            },
+            hrefRouting(){
+                // 새로고침 발생
+                window.location.href = "/practice/modeling"
+            },
+            spaRouting1(){
+                // 새로고침 없음(spa : single page application) 보편적으로 많이씀
+                this.$router.push("/practice/modeling")
+            },
+            spaRouting2(){
+                // 새로고침 없음. 이름기반 라우팅팅
+                this.$router.push({name: "ModelingComponent"})
+            },
+            resetModal(){
+                this.trueOrFalse=false
+            }
+        },
     }
 </script>
--- src/router/practiceRouter.js
+++ src/router/practiceRouter.js
@@ -1,8 +1,26 @@
+import ConditionalAxiosComponent from "@/components/practice/ConditionalAxiosComponent.vue";
+import HookComponent from "@/components/practice/HookComponent.vue";
+import ModelingComponent from "@/components/practice/ModelingComponent.vue";
 import VuetifyComponent from "@/components/practice/VuetifyComponent.vue";
 export const practiceRouter = [
     {
         path: '/practice/vuetify',
         name: 'VuetifyComponent',
         component: VuetifyComponent
+    },
+    {
+        path: '/practice/modeling',
+        name: 'ModelingComponent',
+        component: ModelingComponent
+    },
+    {
+        path: '/practice/hooks',
+        name: 'HookComponent',
+        component: HookComponent
+    },
+    {
+        path: '/practice/conditional',
+        name: 'ContionalAxiosComponent',
+        component: ConditionalAxiosComponent
     }
 ]
